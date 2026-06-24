b9a8c0500c85c36678121093e7ed3b182b9a716b
JoaoTavares <joaopedrocastilhotavares@gmail.com>
2026-06-24 21:34:36 +0100

    Implement log writing functionality

--- modules/Core/Logger.ps1
+++ modules/Core/Logger.ps1
@@ -2,10 +2,21 @@ function Write-Log
 {
     # Writes formatted log entries to the OpeOpe Toolkit log file.
     # Supports INFO, SUCCESS, WARNING and ERROR severity levels.
+
     param(
         [string]$Message,
 
         [ValidateSet("INFO","SUCCESS","WARNING","ERROR")]
         [string]$Level
     )
+
+    $TimeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+    $LogEntry = "[$TimeStamp] [$Level] $Message"
+
+    Write-Host $LogEntry
+
+    Add-Content `
+        -Path $Global:LogFilePath `
+        -Value $LogEntry
 }
