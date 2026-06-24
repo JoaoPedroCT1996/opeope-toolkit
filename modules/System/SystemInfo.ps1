23ad199a95bf8fc9c3fe69df57a21fd77575ad5f
JoaoTavares <joaopedrocastilhotavares@gmail.com>
2026-06-24 22:14:28 +0100

    Extend system information collection

--- modules/System/SystemInfo.ps1
+++ modules/System/SystemInfo.ps1
@@ -1,4 +1,3 @@
-
 function Get-SystemInformation
 {
     # Collects basic system information for diagnostics and reporting.
@@ -6,11 +5,33 @@ function Get-SystemInformation
     $ComputerName = $env:COMPUTERNAME
     $CurrentUser = $env:USERNAME
 
+    $OperatingSystem = Get-CimInstance Win32_OperatingSystem
+    $ComputerSystem = Get-CimInstance Win32_ComputerSystem
+
     Write-Log `
         -Message "Computer Name: $ComputerName" `
         -Level INFO
 
     Write-Log `
         -Message "Current User: $CurrentUser" `
+        -Level INFO
+
+    Write-Log `
+        -Message "Operating System: $($OperatingSystem.Caption)" `
+        -Level INFO
+
+    Write-Log `
+        -Message "OS Version: $($OperatingSystem.Version)" `
+        -Level INFO
+
+    # Convert installed physical memory from bytes to gigabytes.
+
+    $TotalRAM = [math]::Round(
+        $ComputerSystem.TotalPhysicalMemory / 1GB,
+        2
+    )
+
+    Write-Log `
+        -Message "Installed RAM: $TotalRAM GB" `
         -Level INFO
 }
