b7cbdd8b6a45d18a1f6c138f019b6a397b97739f
JoaoTavares <joaopedrocastilhotavares@gmail.com>
2026-06-25 16:13:50 +0100

    Implement initial security information collection

--- modules/Security/SecurityInfo.ps1
+++ modules/Security/SecurityInfo.ps1
@@ -6,6 +6,30 @@ function Get-SecurityInformation
         -Namespace ROOT\SecurityCenter2 `
         -ClassName AntiVirusProduct
 
+    $FirewallProfiles = Get-NetFirewallProfile
+
+    #
+    # Validate collected information.
+    #
+
+    if ($null -eq $AntivirusProducts)
+    {
+        Write-Log `
+            -Message "No registered antivirus products found" `
+            -Level WARNING
+
+        return
+    }
+
+    if ($null -eq $FirewallProfiles)
+    {
+        Write-Log `
+            -Message "No firewall profiles found" `
+            -Level WARNING
+
+        return
+    }
+
     #
     # Antivirus
     #
@@ -14,6 +38,21 @@ function Get-SecurityInformation
     {
         Write-Log `
             -Message "Registered Antivirus: $($Antivirus.displayName)" `
+            -Level INFO
+    }
+
+    #
+    # Firewall
+    #
+
+    foreach ($FirewallProfile in $FirewallProfiles)
+    {
+        Write-Log `
+            -Message "Firewall Profile: $($FirewallProfile.Name)" `
+            -Level INFO
+
+        Write-Log `
+            -Message "Firewall Status: $(if ($FirewallProfile.Enabled) { "Enabled" } else { "Disabled" })" `
             -Level INFO
     }
 }
