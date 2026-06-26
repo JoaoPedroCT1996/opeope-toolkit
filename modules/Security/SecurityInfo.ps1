27f50adcd22168a69e4b7a8c172a372f915fb47b
JoaoTavares <joaopedrocastilhotavares@gmail.com>
2026-06-26 15:28:11 +0100

    Enhance security information module with TPM support

--- modules/Security/SecurityInfo.ps1
+++ modules/Security/SecurityInfo.ps1
@@ -1,37 +1,51 @@
-function Get-SecurityInformation
-{
+function Get-SecurityInformation {
     # Collects security information for diagnostics and reporting.
 
-    $AntivirusProducts = Get-CimInstance `
-        -Namespace ROOT\SecurityCenter2 `
-        -ClassName AntiVirusProduct
+    try {
+        $AntivirusProducts = Get-CimInstance `
+            -Namespace ROOT\SecurityCenter2 `
+            -ClassName AntiVirusProduct `
+            -ErrorAction Stop
+    }
+    catch {
+        $AntivirusProducts = $null
+    }
 
-    $FirewallProfiles = Get-NetFirewallProfile
+    try {
+        $FirewallProfiles = Get-NetFirewallProfile `
+            -ErrorAction Stop
+    }
+    catch {
+        $FirewallProfiles = $null
+    }
 
-    try
-    {
+    try {
         $BitLockerVolumes = Get-BitLockerVolume `
             -ErrorAction Stop
     }
-    catch
-    {
+    catch {
         $BitLockerVolumes = $null
+    }
+
+    try {
+        $Tpm = Get-Tpm `
+            -ErrorAction Stop
+    }
+    catch {
+        $Tpm = $null
     }
 
     #
     # Antivirus
     #
 
-    if ($null -eq $AntivirusProducts)
-    {
+    if ($null -eq $AntivirusProducts) {
         Write-Log `
             -Message "No registered antivirus products found" `
             -Level WARNING
     }
-    else
-    {
-        foreach ($Antivirus in $AntivirusProducts)
-        {
+    else {
+        foreach ($Antivirus in $AntivirusProducts) {
             Write-Log `
                 -Message "Registered Antivirus: $($Antivirus.displayName)" `
                 -Level INFO
@@ -42,16 +56,13 @@ function Get-SecurityInformation
     # Firewall
     #
 
-    if ($null -eq $FirewallProfiles)
-    {
+    if ($null -eq $FirewallProfiles) {
         Write-Log `
             -Message "No firewall profiles found" `
             -Level WARNING
     }
-    else
-    {
-        foreach ($FirewallProfile in $FirewallProfiles)
-        {
+    else {
+        foreach ($FirewallProfile in $FirewallProfiles) {
             Write-Log `
                 -Message "Firewall Profile: $($FirewallProfile.Name)" `
                 -Level INFO
@@ -66,16 +77,13 @@ function Get-SecurityInformation
     # BitLocker
     #
 
-    if ($null -eq $BitLockerVolumes)
-    {
+    if ($null -eq $BitLockerVolumes) {
         Write-Log `
             -Message "BitLocker information is unavailable" `
             -Level WARNING
     }
-    else
-    {
-        foreach ($BitLockerVolume in $BitLockerVolumes)
-        {
+    else {
+        foreach ($BitLockerVolume in $BitLockerVolumes) {
             Write-Log `
                 -Message "Drive: $($BitLockerVolume.MountPoint)" `
                 -Level INFO
@@ -96,5 +104,45 @@ function Get-SecurityInformation
                 -Message "Encryption: $($BitLockerVolume.EncryptionPercentage)%" `
                 -Level INFO
         }
+    }
+
+    #
+    # TPM
+    #
+
+    if ($null -eq $Tpm) {
+        Write-Log `
+            -Message "TPM information is unavailable" `
+            -Level WARNING
+    }
+    elseif (-not $Tpm.TpmPresent) {
+        Write-Log `
+            -Message "TPM is not present" `
+            -Level INFO
+    }
+    else {
+        Write-Log `
+            -Message "TPM Present: $($Tpm.TpmPresent)" `
+            -Level INFO
+
+        Write-Log `
+            -Message "TPM Ready: $($Tpm.TpmReady)" `
+            -Level INFO
+
+        Write-Log `
+            -Message "TPM Enabled: $($Tpm.TpmEnabled)" `
+            -Level INFO
+
+        Write-Log `
+            -Message "TPM Activated: $($Tpm.TpmActivated)" `
+            -Level INFO
+
+        Write-Log `
+            -Message "Manufacturer: $($Tpm.ManufacturerIdTxt)" `
+            -Level INFO
+
+        Write-Log `
+            -Message "Manufacturer Version: $($Tpm.ManufacturerVersion)" `
+            -Level INFO
     }
 }
