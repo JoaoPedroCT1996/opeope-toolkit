5480d7d27567ee027b950b4f904aa419395d13b7
JoaoTavares <joaopedrocastilhotavares@gmail.com>
2026-06-27 20:06:38 +0100

    Implement system health checks

--- modules/Monitoring/HealthCheck.ps1
+++ modules/Monitoring/HealthCheck.ps1
@@ -5,16 +5,31 @@ function Start-HealthCheck {
         -Message "Starting system health check..." `
         -Level INFO
 
-    #
-    # Health checks
-    #
+    Test-SystemHealth
 
-    # Test-SystemHealth
-    # Test-NetworkHealth
-    # Test-SecurityHealth
-    # Test-StorageHealth
+    Test-StorageHealth
+
+    Test-NetworkHealth
+
+    Test-SecurityHealth
 
     Write-Log `
         -Message "System health check completed." `
         -Level INFO
+}
+
+function Test-SystemHealth {
+    # Performs system health checks.
+}
+
+function Test-StorageHealth {
+    # Performs storage health checks.
+}
+
+function Test-NetworkHealth {
+    # Performs network health checks.
+}
+
+function Test-SecurityHealth {
+    # Performs security health checks.
 }
